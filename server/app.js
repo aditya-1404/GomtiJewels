51bca6beacbde138c57190575aeb7aaec3e7bfee
Aditya Srivastava <91134591+aditya-1404@users.noreply.github.com>
2023-07-19 12:22:36 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: true,
     credentials: true,
 }));
 app.use('/',require("./routes/add"))
