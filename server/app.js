eee755a5a5eefadce60ed3424dc9f7dc40a15181
aditya-1404 <aditya1442002@gmail.com>
2024-08-01 01:39:49 +0530

    fix

--- server/app.js
+++ server/app.js
@@ -13,13 +13,13 @@ app.use(express.json())
 const path=require('path')
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://gomti-jewels-tq9c.vercel.app");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "https://gomti-jewels-tq9c.vercel.app");
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     next();
+//   });
 app.set("trust proxy", 1)
 const corsOptions = {
     origin: "https://gomti-jewels-tq9c.vercel.app", // Allow requests only from this origin
